ebb9d97d6af294b35b0e7d55749f7c3d6ec0f92a
AhMohsen <ahmad.mohsen@robustastudio.com>
2020-04-08 11:34:51 +0200

    validate subnet address uniqueness

--- subnet/models.py
+++ subnet/models.py
@@ -5,7 +5,7 @@ class Subnets(models.Model):
     # subnet name
     name = models.CharField(max_length=255, null=False)
     # network address
-    network_address = models.CharField(max_length=255, null=False)
+    network_address = models.CharField(max_length=30, unique=True, null=False)
 
     def __str__(self):
         return "{} - {}".format(self.name, self.network_address)
